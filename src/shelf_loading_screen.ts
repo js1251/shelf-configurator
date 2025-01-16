32a65b1f5de4bc33a5c10f0d7133f98093dc4021
js1251 <sailer.jakob@web.de>
2025-01-16 11:24:30 +0100

    Feature: Loading screen transition

--- src/shelf_loading_screen.ts
+++ src/shelf_loading_screen.ts
@@ -16,8 +16,9 @@ export class ShelfLoadingScreen implements ILoadingScreen {
         const sizePlus = size + 1;
         const sizeHalf = sizePlus / 2;
         const radius = (sizePlus - strokeWidth) / 2;
+        
         const loadingDiv = document.createElement("div");
-        loadingDiv.innerHTML = `<svg class="shelf-spinner" width="${size}px" height="${size}px" viewBox="0 0 ${sizePlus} ${sizePlus}" xmlns="http://www.w3.org/2000/svg">
+        loadingDiv.innerHTML += `<svg class="shelf-spinner" width="${size}px" height="${size}px" viewBox="0 0 ${sizePlus} ${sizePlus}" xmlns="http://www.w3.org/2000/svg">
                                     <circle class="shelf-spinner-path" fill="none" stroke-width="6" stroke-linecap="round" cx="${sizeHalf}" cy="${sizeHalf}" r="${radius}"></circle>
                                 </svg>`;
         loadingDiv.id = "shelf-loading-container";
@@ -27,7 +28,13 @@ export class ShelfLoadingScreen implements ILoadingScreen {
     public hideLoadingUI() {
         const loadingDiv = document.getElementById("shelf-loading-container");
         if (loadingDiv) {
-            document.body.removeChild(loadingDiv);
+            // set opacity to 0 to trigger the CSS animation
+            loadingDiv.style.opacity = "0";
+            loadingDiv.innerHTML = "";
+
+            loadingDiv.addEventListener('transitionend', () => {
+                loadingDiv.remove();
+            });
         }
     }
 }
